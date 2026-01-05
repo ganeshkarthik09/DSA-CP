bdb7a681dbfd9089326200dcb6e83c1b4492a6d4
Vislavath <vislavathganeshkarthik@gmail.com>
2026-01-05 12:29:09 +0530

    Time: 182 ms (61.07%), Space: 23.2 MB (41.12%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool isPrime(int n) {
-        if (n < 2)
-            return false;
-        for (int i = 2; i * i <= n; ++i)
-            if (n % i == 0)
-                return false;
-        return true;
-    }
-
     vector<int> closestPrimes(int left, int right) {
-        vector<int> primes;
-        for (int i = left; i <= right; ++i) {
-            if (isPrime(i)) {
-                primes.push_back(i);
+        vector<bool> isPrime(right+1, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i * i <= right; ++i) {
+            if (isPrime[i]) {
+                for (int j = i*i; j <= right; j += i)
+                    isPrime[j] = false;
             }
+        }
+        vector<int> primes;
+        for (int i = max(2, left); i <= right; ++i) {
+            if (isPrime[i]) primes.push_back(i);
         }
         int k = primes.size();
-        if(k== 0 || k == 1) return {-1,-1};
-        int p1=primes[0],p2=primes[1];
-        int dis = abs(primes[0] - primes[1]);
-        for (int i = 1; i < k-1; ++i) {
-            int temp = abs(primes[i + 1] - primes[i]);
-            if(temp < dis)
-            {
-                dis = temp;
-                p2 = primes[i+1];
-                p1 = primes[i];
-            }
+        if (k < 2) return {-1, -1};
 
+        int p1 = primes[0], p2 = primes[1];
+        int dis = p2 - p1;
+        for (int i = 1; i < k-1; ++i) {
+            int temp = primes[i+1] - primes[i];
+            if (temp < dis) {
+                dis = temp;
+                p1 = primes[i];
+                p2 = primes[i+1];
+            }
         }
-        return {p1,p2};
+        return {p1, p2};
     }
 };
